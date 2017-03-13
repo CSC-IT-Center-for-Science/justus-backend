0cb7510078209b57fe49eae895f4c9441473f401
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-03-13 11:14:09 +0200

    Update PHP API for PostgreSQL.

---
+++ sql/tables.sql
@@ -0,0 +1,137 @@
+﻿-- Table: julkaisu
+
+-- DROP TABLE julkaisu;
+
+CREATE TABLE julkaisu
+(
+  id bigserial NOT NULL,
+  organisaatiotunnus text,
+  julkaisutyyppi text,
+  julkaisuvuosi integer,
+  julkaisunnimi text,
+  tekijat text,
+  julkaisuntekijoidenlukumaara integer,
+  konferenssinvakiintunutnimi text,
+  emojulkaisunnimi text,
+  isbn text,
+  emojulkaisuntoimittajat text,
+  lehdenjulkaisusarjannimi text,
+  issn text,
+  volyymi text,
+  numero text,
+  sivut text,
+  artikkelinumero text,
+  kustantaja text,
+  julkaisunkustannuspaikka text,
+  julkaisunkieli text,
+  julkaisunkansainvalisyys integer,
+  julkaisumaa text,
+  kansainvalinenyhteisjulkaisu integer,
+  yhteisjulkaisuyrityksenkanssa integer,
+  doitunniste text,
+  pysyvaverkkoosoite text,
+  avoinsaatavuus text,
+  julkaisurinnakkaistallenettu integer,
+  rinnakkaistallenetunversionverkkoosoite text,
+  jufotunnus text,
+  jufoluokitus text,
+  CONSTRAINT julkaisu_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE julkaisu
+  OWNER TO appaccount;
+
+
+
+
+-- Table: avainsana
+
+-- DROP TABLE avainsana;
+
+CREATE TABLE avainsana
+(
+  id bigserial NOT NULL,
+  julkaisuid bigint NOT NULL,
+  avainsana text NOT NULL,
+  CONSTRAINT avainsana_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_julkaisu FOREIGN KEY (julkaisuid)
+      REFERENCES julkaisu (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE avainsana
+  OWNER TO appaccount;
+
+
+
+-- Table: tieteenala
+
+-- DROP TABLE tieteenala;
+
+CREATE TABLE tieteenala
+(
+  id bigserial NOT NULL,
+  julkaisuid bigint NOT NULL,
+  tieteenalakoodi text NOT NULL,
+  jnro integer,
+  CONSTRAINT tieteenala_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_julkaisu FOREIGN KEY (julkaisuid)
+      REFERENCES julkaisu (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE tieteenala
+  OWNER TO appaccount;
+
+
+
+
+
+-- Table: organisaatiotekija
+
+-- DROP TABLE organisaatiotekija;
+
+CREATE TABLE organisaatiotekija
+(
+  id bigserial NOT NULL,
+  julkaisuid bigint NOT NULL,
+  etunimet text,
+  sukunimi text,
+  orcid text,
+  CONSTRAINT organisaatiotekija_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_julkaisu FOREIGN KEY (julkaisuid)
+      REFERENCES julkaisu (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE organisaatiotekija
+  OWNER TO appaccount;
+
+
+-- Table: alayksikko
+
+-- DROP TABLE alayksikko;
+
+CREATE TABLE alayksikko
+(
+  id bigserial NOT NULL,
+  organisaatiotekijaid bigint NOT NULL,
+  alayksikko text,
+  CONSTRAINT alayksikko_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_organisaatiotekija FOREIGN KEY (organisaatiotekijaid)
+      REFERENCES organisaatiotekija (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE alayksikko
+  OWNER TO appaccount;
--- src/justus_save.php
+++ src/justus_save.php
@@ -21,11 +21,13 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
 // connect to the database
-$settings = parse_ini_file('/var/www/justus-settings.ini', true);
-$link = mssql_connect($settings['database']['host'], $settings['database']['user'], $settings['database']['pass']);
-if(!mssql_select_db($settings['database']['name'], $link)){
-  die('Something went wrong while connecting to database');
-}
+$settings = parse_ini_file('/etc/justus-backend.ini', true);
+$dbconn = pg_connect(
+  "host=".$settings['database']['host'].
+  " dbname=".$settings['database']['name'].
+  " user=".$settings['database']['user'].
+  " password=".$settings['database']['pass'])
+  or die('Something went wrong while connecting to database: '.pg_last_error());
 
 // retrieve the table and key from the path
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
@@ -33,7 +35,7 @@ $key = array_shift($request)+0;
 
 // escape the columns and values from the input object
 $columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-$values = array_map(function ($value) use ($link) {
+$values = array_map(function ($value) use ($dbconn) {
   if ($value===null) return null;
   // escape str
   $value = stripslashes($value);
@@ -48,58 +50,52 @@ $set = '';
 $sqlcolumns = '';
 $sqlvalues = '';
 for ($i=0;$i<count($columns);$i++) {
-  $set.=($i>0?',':'').'['.$columns[$i].']=';
-  $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
-  $sqlcolumns.=($i>0?',':'').'['.$columns[$i].']';
+  $set.=($i>0?',':'').$columns[$i].'=';
+  $set.=($values[$i]===null?'NULL':"'".$values[$i]."'");
+  $sqlcolumns.=($i>0?',':'').$columns[$i];
   $sqlvalues.=($i>0?',':'').($values[$i]===null?'NULL':"'".$values[$i]."'");
 }
 
 // create SQL based on HTTP method
 switch ($method) {
   case 'GET':
-    $sql = "select * from [$table]".($key?" WHERE id=$key":''); break;
+    $sql = "select * from \"$table\"".($key?" WHERE id=$key":'');
+    break;
   case 'PUT':
-    $sql = "update [$table] set $set where id=$key"; break;
+    $sql = "update \"$table\" set $set where id=$key";
+    break;
   case 'POST':
-    $sql = "insert into [$table] ($sqlcolumns) values ($sqlvalues)"; break;
+    $sql = "insert into \"$table\" ($sqlcolumns) values ($sqlvalues) returning id";
+    break;
   case 'DELETE':
-    $sql = "delete [$table] where id=$key"; break;
+    $sql = "delete from \"$table\" where id=$key";
+    break;
 }
 
 // excecute SQL statement
-// lisää turvallisuutta käyttämällä sp_executesql proseduuria
-// TODO pitää tehdä lisää escapea, tai tuo params...
-//$dbsql = "EXEC sp_executesql
-//          N'$sql'";//,
-          //N'$params',
-          //$paramslist";
-$result = mssql_query($sql,$link);
+$result = pg_query($dbconn,$sql);
 
 // die if SQL statement failed
 if (!$result) {
   http_response_code(404);
-  die(mssql_get_last_message());
+  die(pg_last_notice($dbconn));
 }
 
 // print results, insert id or affected row count
 if ($method == 'GET') {
   if (!$key) echo '[';
-  for ($i=0;$i<mssql_num_rows($result);$i++) {
-    echo ($i>0?',':'').json_encode(mssql_fetch_object($result));
+  for ($i=0;$i<pg_num_rows($result);$i++) {
+    echo ($i>0?',':'').json_encode(pg_fetch_object($result));
   }
   if (!$key) echo ']';
 } elseif ($method == 'POST') {
-  $insert_result = mssql_query('select SCOPE_IDENTITY() AS last_insert_id') or die('Scope Query failed');
-  $insert_result = mssql_fetch_object($insert_result);
-  mssql_free_result($insert_result);
-  echo $insert_result->last_insert_id;
+  echo pg_fetch_object($result)->id;
 } else {
-  echo mssql_rows_affected($link);
+  echo pg_affected_rows($result);
 }
 
 // clean up
-mssql_free_result($result);
+pg_free_result($result);
 // close connection
-mssql_close($link);
-
+pg_close($dbconn);
 ?>
