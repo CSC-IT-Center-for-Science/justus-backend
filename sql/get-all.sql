372cd1e64ea2bd922bf0e71081be62f469bf5fa3
Lauri Jokipii <lauri.jokipii@csc.fi>
2017-04-22 13:06:56 +0300

    Add script for view creation for backend. Also add a helping script.

---
+++ sql/get-all.sql
@@ -0,0 +1,34 @@
+﻿select julkaisu.id,julkaisutyyppi,julkaisuvuosi,julkaisunnimi
+,array_to_json((
+  select array_agg(r.avainsana)
+  from (
+    select avainsana
+    from avainsana
+    where julkaisuid=julkaisu.id
+  ) as r
+)) as avainsanat
+,array_to_json((
+  select array_agg(r)
+  from (
+    select tieteenalakoodi,jnro
+    from tieteenala
+    where julkaisuid=julkaisu.id
+  ) as r
+)) as tieteenalat
+,array_to_json((
+  select array_agg(r)
+  from (
+    select etunimet,sukunimi,orcid
+    ,array_to_json((
+      select array_agg(rr.alayksikko)
+      from (
+        select alayksikko
+        from alayksikko
+        where organisaatiotekijaid=organisaatiotekija.id
+      ) as rr
+    )) as alayksikot
+    from organisaatiotekija
+    where julkaisuid=julkaisu.id
+  ) as r
+)) as organisaationtekijat
+from julkaisu
