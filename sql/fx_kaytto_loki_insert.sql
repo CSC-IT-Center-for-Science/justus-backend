4118b3b672099f33a4711cbd84073086fb8ab1b5
Raine Rapo <raine.rapo@csc.fi>
2017-10-20 15:12:53 +0300

    kaytto_loki toiminnalisuuden lisääminen

---
+++ sql/fx_kaytto_loki_insert.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION kaytto_loki_insert()
+  RETURNS trigger AS
+$$
+BEGIN
+         UPDATE kaytto_loki set luonti_pvm = current_timestamp
+         WHERE id = NEW.id;
+ 
+    RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+grant execute on function kaytto_loki_insert to appaccount;
+
