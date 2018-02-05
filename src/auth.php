df9f9407e98cd22c6cc02c884da74c8a158d7d0a
Olli Eskola <olli@localhost.localdomain>
2018-02-05 11:28:52 +0200

    mpkk added again

--- src/auth.php
+++ src/auth.php
@@ -46,6 +46,7 @@ if (array_key_exists("shib-group",$_SERVER)) {
         '@lamk.fi' => '02470',
         '@laurea.fi' => '02629',
         '@metropolia.fi' => '10065',
+        '@mil.fi' => '02358',
         '@samk.fi' => '02507',
         '@seamk.fi' => '02472',
         '@tamk.fi' => '02630',
